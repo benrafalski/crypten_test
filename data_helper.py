000653f841538305d5a3fdba5451dddf2272c96b
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-12-30 09:54:50 -0700

    cleaned up data, finished data_helper for 2 clients, + .gitignore

--- data_helper.py
+++ data_helper.py
@@ -13,28 +13,72 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset, DataLoader
 from statistics import mean
 
-CLIENTS = 2
 
-dataset = datasets.MNIST('~/mnist.pth', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ]))
+def _get_norm_mnist(dir, reduced=None, binary=False):
+    """Downloads and normalizes mnist"""
+    mnist_train = datasets.MNIST(dir, download=True, train=True)
+    mnist_test = datasets.MNIST(dir, download=True, train=False)
 
-def make_dataset(size):
-    indices = np.arange(len(dataset))
-    train_indices, test_indices = train_test_split(indices, train_size=size*10, stratify=dataset.targets)
+    # compute normalization factors
+    data_all = torch.cat([mnist_train.data, mnist_test.data]).float()
+    data_mean, data_std = data_all.mean(), data_all.std()
+    tensor_mean, tensor_std = data_mean.unsqueeze(0), data_std.unsqueeze(0)
 
-    train_dataset = Subset(dataset, train_indices)
-    test_dataset = Subset(dataset, test_indices)
+    # normalize
+    mnist_train_norm = transforms.functional.normalize(
+        mnist_train.data.float(), tensor_mean, tensor_std
+    )
+    mnist_test_norm = transforms.functional.normalize(
+        mnist_test.data.float(), tensor_mean, tensor_std
+    )
 
-    train_loader = DataLoader(train_dataset, shuffle=True, num_workers=0, batch_size=10)
-    test_loader = DataLoader(test_dataset, shuffle=False, num_workers=0, batch_size=10)
+    # create a reduced dataset if required
+    if reduced is not None:
+        mnist_norm = (mnist_train_norm[:reduced], mnist_test_norm[:reduced])
+        mnist_labels = (mnist_train.targets[:reduced], mnist_test.targets[:reduced])
+    else:
+        mnist_norm = (mnist_train_norm, mnist_test_norm)
+        mnist_labels = (mnist_train.targets, mnist_test.targets)
+    return mnist_norm, mnist_labels
 
-    return train_loader, test_loader
+
+def split_features(
+    split=0.5, dir="tmp", party1="alice", party2="bob", reduced=None, binary=False
+):
+    """Splits features between Party 1 and Party 2"""
+    mnist_norm, mnist_labels = _get_norm_mnist(dir, reduced, binary)
+    mnist_train_norm, mnist_test_norm = mnist_norm
+    mnist_train_labels, mnist_test_labels = mnist_labels
+
+    num_features = mnist_train_norm.shape[1]
+    split_point = int(split * num_features)
+
+    party1_train = mnist_train_norm[:, :, :split_point]
+    party2_train = mnist_train_norm[:, :, split_point:]
+    party1_test = mnist_test_norm[:, :, :split_point]
+    party2_test = mnist_test_norm[:, :, split_point:]
+
+    torch.save(party1_train, os.path.join(dir, party1 + "_train.pth"))
+    torch.save(party2_train, os.path.join(dir, party2 + "_train.pth"))
+    torch.save(party1_test, os.path.join(dir, party1 + "_test.pth"))
+    torch.save(party2_test, os.path.join(dir, party2 + "_test.pth"))
+    torch.save(mnist_train_labels, os.path.join(dir, "train_labels.pth"))
+    torch.save(mnist_test_labels, os.path.join(dir, "test_labels.pth"))
 
 
-for i in range(CLIENTS):
-    a, b = make_dataset(6000//CLIENTS)
+
+def main():
+    split_features(
+            split=0.72,
+            dir="tmp",
+            party1="alice",
+            party2="bob",
+            reduced=100,
+            binary=False,
+        )
+
+if __name__ == "__main__":
+    main()
     
 
 
